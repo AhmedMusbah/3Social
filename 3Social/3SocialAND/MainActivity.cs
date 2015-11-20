2988812104fc0748ded234ab115a48f5fc56a2ac
Ahmed Musbah <ahmedmusbah@outlook.com>
2015-11-20 18:15:34 +0200

    v1

---
+++ 3Social/3SocialAND/MainActivity.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace _3SocialAND
+{
+    [Activity(Label = "3Social", Icon = "@drawable/social", Theme = "@android:style/Theme.Holo.Light")]
+    public class MainActivity : Activity
+    {
+        bool facebook = false;
+        bool twitter = false;
+        bool instagram = false;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+        }
+
+        /*
+		 * attach the menu to the menu button of the device
+		 * for this activity
+		 */
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            base.OnCreateOptionsMenu(menu);
+
+            MenuInflater inflater = this.MenuInflater;
+
+            inflater.Inflate(Resource.Menu.mainmenu, menu);
+
+            return true;
+        }
+
+        /// <param name="item">The menu item that was selected.</param>
+		/// <summary>
+		/// This hook is called whenever an item in your options menu is selected.
+		/// </summary>
+		/// <returns>To be added.</returns>
+		public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            base.OnOptionsItemSelected(item);
+
+            WebView webFacebook = FindViewById<WebView>(Resource.Id.webFacebook);
+            webFacebook.SetWebViewClient(new WebViewClient());
+            webFacebook.Visibility = ViewStates.Gone;
+            webFacebook.Settings.JavaScriptEnabled = true;
+
+            WebView webTwitter = FindViewById<WebView>(Resource.Id.webTwitter);
+            webTwitter.SetWebViewClient(new WebViewClient());
+            webTwitter.Visibility = ViewStates.Gone;
+            webTwitter.Settings.JavaScriptEnabled = true;
+
+            WebView webInstagram = FindViewById<WebView>(Resource.Id.webInstagram);
+            webInstagram.SetWebViewClient(new WebViewClient());
+            webInstagram.Visibility = ViewStates.Gone;
+            webInstagram.Settings.JavaScriptEnabled = true;
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.btnFacebook:
+                    {
+                        webFacebook.Visibility = ViewStates.Visible;
+                        webTwitter.Visibility = ViewStates.Gone;
+                        webInstagram.Visibility = ViewStates.Gone;
+
+                        if (facebook == false)
+                        {
+                            webFacebook.LoadUrl("http://www.facebook.com");
+
+                            facebook = true;
+                        }
+
+                        break;
+                    }
+
+                case Resource.Id.btnTwitter:
+                    {
+                        webFacebook.Visibility = ViewStates.Gone;
+                        webTwitter.Visibility = ViewStates.Visible;
+                        webInstagram.Visibility = ViewStates.Gone;
+
+                        if (twitter == false)
+                        {
+                            webTwitter.LoadUrl("http://www.twitter.com");
+
+                            twitter = true;
+                        }
+
+                        break;
+                    }
+
+                case Resource.Id.btnInstagram:
+                    {
+                        webFacebook.Visibility = ViewStates.Gone;
+                        webTwitter.Visibility = ViewStates.Gone;
+                        webInstagram.Visibility = ViewStates.Visible;
+
+                        if (instagram == false)
+                        {
+                            webInstagram.LoadUrl("http://www.instagram.com");
+
+                            instagram = true;
+                        }
+
+                        break;
+                    }
+            }
+
+            return true;
+        }
+    }
+}
+
---
+++ 3Social/3SocialWIN/MainPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _3SocialWIN
+{
+    public sealed partial class MainPage : Page
+    {
+        bool facebook = false;
+        bool twitter = false;
+        bool instagram = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnFacebook_Click(null, null);
+        }
+
+        private async void btnFacebook_Click(object sender, RoutedEventArgs e)
+        {
+            stkFacebook.Visibility = Visibility.Visible;
+            stkTwitter.Visibility = Visibility.Collapsed;
+            stkInstagram.Visibility = Visibility.Collapsed;
+            btnFacebook.IsEnabled = false;
+            btnTwitter.IsEnabled = true;
+            btnInstagram.IsEnabled = true;
+
+            if (facebook == false)
+            {
+                try
+                {
+                    var url = new Uri("https://www.facebook.com/");
+                    webFacebook.Navigate(url);
+                }
+                catch (Exception ex)
+                {
+                    var cd = new ContentDialog();
+                    cd.Title = "Check your Internet connectivity.";
+                    cd.Content = ex.ToString();
+                    await cd.ShowAsync();
+                }
+
+                facebook = true;
+            }
+        }
+
+        private async void btnTwitter_Click(object sender, RoutedEventArgs e)
+        {
+            stkFacebook.Visibility = Visibility.Collapsed;
+            stkTwitter.Visibility = Visibility.Visible;
+            stkInstagram.Visibility = Visibility.Collapsed;
+            btnFacebook.IsEnabled = true;
+            btnTwitter.IsEnabled = false;
+            btnInstagram.IsEnabled = true;
+
+            if (twitter == false)
+            {
+                try
+                {
+                    var url = new Uri("https://www.twitter.com/");
+                    webTwitter.Navigate(url);
+                }
+                catch (Exception ex)
+                {
+                    var cd = new ContentDialog();
+                    cd.Title = "Check your Internet connectivity.";
+                    cd.Content = ex.ToString();
+                    await cd.ShowAsync();
+                }
+
+                twitter = true;
+            }
+        }
+
+        private async void btnInstagram_Click(object sender, RoutedEventArgs e)
+        {
+            stkFacebook.Visibility = Visibility.Collapsed;
+            stkTwitter.Visibility = Visibility.Collapsed;
+            stkInstagram.Visibility = Visibility.Visible;
+            btnFacebook.IsEnabled = true;
+            btnTwitter.IsEnabled = true;
+            btnInstagram.IsEnabled = false;
+
+            if (instagram == false)
+            {
+                try
+                {
+                    var url = new Uri("https://www.instagram.com/");
+                    webInstagram.Navigate(url);
+                }
+                catch (Exception ex)
+                {
+                    var cd = new ContentDialog();
+                    cd.Title = "Check your Internet connectivity.";
+                    cd.Content = ex.ToString();
+                    await cd.ShowAsync();
+                }
+
+                instagram = true;
+            }
+        }
+    }
+}
